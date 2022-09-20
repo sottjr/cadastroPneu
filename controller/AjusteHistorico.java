c5e9c6b4d1486f7724e77e9d1cae180a160d525a
sottjr <sottjr@gmail.com>
2022-09-20 18:04:26 -0300

    chore: created class Ajuste Historic first commit

---
+++ controller/AjusteHistorico.java
@@ -0,0 +1,27 @@
+package com.lecom.workflow.cadastros.pneus.controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.apache.log4j.Logger;
+import org.apache.http.client.ClientProtocolException;
+
+import com.lecom.tecnologia.db.DBUtils;
+import com.lecom.workflow.cadastros.common.util.Funcoes;
+import com.lecom.workflow.vo.IntegracaoVO;
+
+import br.com.lecom.atos.servicos.annotation.Execution;
+import br.com.lecom.atos.servicos.annotation.IntegrationModule;
+import br.com.lecom.atos.servicos.annotation.Version;
+
+public class AjusteHistorico {
+
+}
